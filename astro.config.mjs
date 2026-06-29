d9c9e2f024e3942d0e6299d4ee586a721dd7161b
Aissa <79700363+aissa-m@users.noreply.github.com>
2026-06-29 15:32:14 +0200

    Simplify Astro config and ensure image assets are published

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://negoce-rim.netlify.app',
-  publicDir: 'img',
-  integrations: [sitemap()],
-  output: 'static'
+  publicDir: './img',
+  output: 'static',
+  build: {
+    format: 'directory'
+  }
 });
